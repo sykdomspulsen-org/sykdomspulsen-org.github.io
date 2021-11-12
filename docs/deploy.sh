ae284eb3f23f9fd868726597cd2e95e0776d69d4
Richard Aubrey White <RichardAubrey.White@fhi.no>
2021-11-12 08:58:16 +0100

    Initial docs subtree commit

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 Rscript -e "rmarkdown::render_site('.')"
-git fetch origin gh-pages
 git add docs && git commit -m "Initial docs subtree commit"
 git subtree push --prefix docs origin gh-pages
-rm -rf docs
 
