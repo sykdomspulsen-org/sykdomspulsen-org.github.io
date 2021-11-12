c328e085deb9ea4861a89d0fb9dcdb02fb7ef9a9
Richard Aubrey White <RichardAubrey.White@fhi.no>
2021-11-12 11:14:34 +0100

    fix

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,7 @@
 
 Rscript -e "rmarkdown::render_site('.')"
 git add docs && git commit -m "Initial docs subtree commit"
-git subtree push --prefix docs origin gh-pages
-
+git push origin `git subtree split --prefix docs main`:gh-pages --force
+git reset --soft HEAD^
+git restore --staged .
+rm -rf docs
